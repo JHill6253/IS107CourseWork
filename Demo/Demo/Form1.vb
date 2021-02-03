6a0d592ae64246cddac62bc210b1b3060ed2fe7f
John <johnhill6253@gmail.com>
2021-02-03 11:47:47 -0600

    First commit

---
+++ Demo/Demo/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles RdBtn.Click
+        TextBox1.ForeColor = Color.Red
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        TextBox1.ForeColor = Color.Red
+    End Sub
+
+    Private Sub TextBox1_Leave(sender As Object, e As EventArgs) Handles TextBox1.Leave
+        TextBox1.ForeColor = Color.Red
+    End Sub
+End Class
