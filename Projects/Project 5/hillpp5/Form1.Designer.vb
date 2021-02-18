d80f32a2a7ae6cd71186ee1f4d292ed2d269514e
John <johnhill6253@gmail.com>
2021-02-17 22:43:41 -0600

    Project 5 Completion

---
+++ Projects/Project 5/hillpp5/Form1.Designer.vb
@@ -0,0 +1,198 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.lblAddress = New System.Windows.Forms.Label()
+        Me.lblCity = New System.Windows.Forms.Label()
+        Me.lblChairs = New System.Windows.Forms.Label()
+        Me.lblSofas = New System.Windows.Forms.Label()
+        Me.txtCustomerName = New System.Windows.Forms.TextBox()
+        Me.txtAddress = New System.Windows.Forms.TextBox()
+        Me.txtCity = New System.Windows.Forms.TextBox()
+        Me.txtChairs = New System.Windows.Forms.TextBox()
+        Me.txtSofas = New System.Windows.Forms.TextBox()
+        Me.lstOutput = New System.Windows.Forms.ListBox()
+        Me.btnProcess = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnQuit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Location = New System.Drawing.Point(8, 20)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(155, 15)
+        Me.lblName.TabIndex = 0
+        Me.lblName.Text = "Customer name: (Last, First)"
+        Me.lblName.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'lblAddress
+        '
+        Me.lblAddress.AutoSize = True
+        Me.lblAddress.Location = New System.Drawing.Point(110, 50)
+        Me.lblAddress.Name = "lblAddress"
+        Me.lblAddress.Size = New System.Drawing.Size(52, 15)
+        Me.lblAddress.TabIndex = 1
+        Me.lblAddress.Text = "Address:"
+        '
+        'lblCity
+        '
+        Me.lblCity.AutoSize = True
+        Me.lblCity.Location = New System.Drawing.Point(78, 77)
+        Me.lblCity.Name = "lblCity"
+        Me.lblCity.Size = New System.Drawing.Size(89, 15)
+        Me.lblCity.TabIndex = 2
+        Me.lblCity.Text = "City, State, Zip :"
+        '
+        'lblChairs
+        '
+        Me.lblChairs.AutoSize = True
+        Me.lblChairs.Location = New System.Drawing.Point(15, 103)
+        Me.lblChairs.Name = "lblChairs"
+        Me.lblChairs.Size = New System.Drawing.Size(150, 15)
+        Me.lblChairs.TabIndex = 3
+        Me.lblChairs.Text = "Number of Chairs Ordered:"
+        '
+        'lblSofas
+        '
+        Me.lblSofas.AutoSize = True
+        Me.lblSofas.Location = New System.Drawing.Point(15, 129)
+        Me.lblSofas.Name = "lblSofas"
+        Me.lblSofas.Size = New System.Drawing.Size(148, 15)
+        Me.lblSofas.TabIndex = 4
+        Me.lblSofas.Text = "Number of Sofas Ordered :"
+        '
+        'txtCustomerName
+        '
+        Me.txtCustomerName.Location = New System.Drawing.Point(165, 17)
+        Me.txtCustomerName.Name = "txtCustomerName"
+        Me.txtCustomerName.Size = New System.Drawing.Size(150, 23)
+        Me.txtCustomerName.TabIndex = 5
+        '
+        'txtAddress
+        '
+        Me.txtAddress.Location = New System.Drawing.Point(165, 47)
+        Me.txtAddress.Name = "txtAddress"
+        Me.txtAddress.Size = New System.Drawing.Size(150, 23)
+        Me.txtAddress.TabIndex = 6
+        '
+        'txtCity
+        '
+        Me.txtCity.Location = New System.Drawing.Point(165, 74)
+        Me.txtCity.Name = "txtCity"
+        Me.txtCity.Size = New System.Drawing.Size(150, 23)
+        Me.txtCity.TabIndex = 7
+        '
+        'txtChairs
+        '
+        Me.txtChairs.Location = New System.Drawing.Point(165, 100)
+        Me.txtChairs.Name = "txtChairs"
+        Me.txtChairs.Size = New System.Drawing.Size(150, 23)
+        Me.txtChairs.TabIndex = 8
+        '
+        'txtSofas
+        '
+        Me.txtSofas.Location = New System.Drawing.Point(165, 126)
+        Me.txtSofas.Name = "txtSofas"
+        Me.txtSofas.Size = New System.Drawing.Size(150, 23)
+        Me.txtSofas.TabIndex = 9
+        '
+        'lstOutput
+        '
+        Me.lstOutput.FormattingEnabled = True
+        Me.lstOutput.ItemHeight = 15
+        Me.lstOutput.Location = New System.Drawing.Point(335, 12)
+        Me.lstOutput.Name = "lstOutput"
+        Me.lstOutput.Size = New System.Drawing.Size(162, 139)
+        Me.lstOutput.TabIndex = 10
+        '
+        'btnProcess
+        '
+        Me.btnProcess.Location = New System.Drawing.Point(119, 183)
+        Me.btnProcess.Name = "btnProcess"
+        Me.btnProcess.Size = New System.Drawing.Size(120, 22)
+        Me.btnProcess.TabIndex = 11
+        Me.btnProcess.Text = "Process Order"
+        Me.btnProcess.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(245, 184)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(120, 22)
+        Me.btnClear.TabIndex = 12
+        Me.btnClear.Text = "Clear Order Form"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnQuit
+        '
+        Me.btnQuit.Location = New System.Drawing.Point(377, 184)
+        Me.btnQuit.Name = "btnQuit"
+        Me.btnQuit.Size = New System.Drawing.Size(120, 22)
+        Me.btnQuit.TabIndex = 13
+        Me.btnQuit.Text = "Quit"
+        Me.btnQuit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(524, 293)
+        Me.Controls.Add(Me.btnQuit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnProcess)
+        Me.Controls.Add(Me.lstOutput)
+        Me.Controls.Add(Me.txtSofas)
+        Me.Controls.Add(Me.txtChairs)
+        Me.Controls.Add(Me.txtCity)
+        Me.Controls.Add(Me.txtAddress)
+        Me.Controls.Add(Me.txtCustomerName)
+        Me.Controls.Add(Me.lblSofas)
+        Me.Controls.Add(Me.lblChairs)
+        Me.Controls.Add(Me.lblCity)
+        Me.Controls.Add(Me.lblAddress)
+        Me.Controls.Add(Me.lblName)
+        Me.Name = "Form1"
+        Me.Text = "Invoice"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblName As Label
+    Friend WithEvents lblAddress As Label
+    Friend WithEvents lblCity As Label
+    Friend WithEvents lblChairs As Label
+    Friend WithEvents lblSofas As Label
+    Friend WithEvents txtCustomerName As TextBox
+    Friend WithEvents txtAddress As TextBox
+    Friend WithEvents txtCity As TextBox
+    Friend WithEvents txtChairs As TextBox
+    Friend WithEvents txtSofas As TextBox
+    Friend WithEvents lstOutput As ListBox
+    Friend WithEvents btnProcess As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnQuit As Button
+End Class
---
+++ Projects/Project 5/hillpp5/Form1.vb
@@ -0,0 +1,68 @@
+﻿Public Class Form1
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles lblAddress.Click
+
+    End Sub
+
+    Private Sub btnProcess_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnProcess.Click
+        Dim lastName, firstName, address, city As String
+        Dim name() As String
+        Dim chairs, sofas As Integer
+        Dim InvoiceId As String
+        Dim cost, salesTax, TotalCost As Double
+
+        InputData(name, address, city, chairs, sofas)
+        ProcessData(name, lastName, firstName, city, InvoiceId)
+        CalculateCost(chairs, sofas, cost, salesTax, TotalCost)
+
+        lstOutput.Items.Clear()
+        lstOutput.Items.Add("Invoice Number:" & InvoiceId)
+        lstOutput.Items.Add("")
+        lstOutput.Items.Add("Name:" & firstName & " " & lastName)
+        lstOutput.Items.Add("Address:" & address)
+        lstOutput.Items.Add("City:" & city)
+        lstOutput.Items.Add(" ")
+        lstOutput.Items.Add("Cost:" & FormatCurrency(cost))
+        lstOutput.Items.Add("Sales Tax:" & FormatCurrency(salesTax))
+        lstOutput.Items.Add("---------")
+        lstOutput.Items.Add("Total Cost:" & FormatCurrency(TotalCost))
+    End Sub
+    Public Sub InputData(ByRef name() As String, ByRef address As String, ByRef city As String, ByRef chairs As Integer, ByRef sofas As Integer)
+        name = txtCustomerName.Text.Split(","c)
+        address = txtAddress.Text
+        city = txtCity.Text
+        chairs = CInt(txtChairs.Text)
+        sofas = CInt(txtSofas.Text)
+    End Sub
+    Public Sub ProcessData(ByVal name() As String, ByRef lastName As String, ByRef firstname As String, ByVal city As String, ByRef invoiceID As String)
+        lastName = name(0)
+        firstname = name(1)
+        Dim length = city.Length
+        invoiceID = lastName.Substring(0, 2).ToUpper
+        invoiceID += city.Substring(length - 4, 4)
+    End Sub
+
+    Public Sub CalculateCost(ByVal chairs As Integer, ByVal sofas As Integer, ByRef cost As Double, ByRef salesTax As Double, ByRef TotalCost As Double)
+        Dim chairCost As Double = 350
+        Dim sofaCost As Double = 925
+        cost = (chairs * chairCost) + (sofas * sofaCost)
+        salesTax = cost * 0.05
+        TotalCost = cost + salesTax
+    End Sub
+
+    Private Sub btnClear_Click(ByVal sender As System.Object, e As System.EventArgs) Handles btnClear.Click
+        txtCustomerName.Clear()
+        txtAddress.Clear()
+        txtCity.Clear()
+        txtChairs.Clear()
+        txtSofas.Clear()
+        lstOutput.Items.Clear()
+    End Sub
+
+    Private Sub btnQuit_Click(ByVal sender As System.Object, e As System.EventArgs) Handles btnQuit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub lblName_Click(sender As Object, e As EventArgs) Handles lblName.Click
+
+    End Sub
+End Class
